1406f16ded1c4bb32846700ca121d7ff0e29baed
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-07-20 23:02:23 +0200

    updated

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,8 @@
-def pow(a, b):
+def pow(a, b): 
     result = 1
     if b < 0:
         a = 1/ a
-        b = abs(b)    
+        b = abs(b)     
     for i in range(b): 
         result = result * a         
-    return result
+    return round(result, 20)  # Round to 20 decimal places
