016e8fc5014f7ff7ff6a2ef42b063308a83208fb
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-07-20 23:01:05 +0200

    updated

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,5 +1,8 @@
 def pow(a, b):
     result = 1
-    for i in range(b):
-        result = result * a        
+    if b < 0:
+        a = 1/ a
+        b = abs(b)    
+    for i in range(b): 
+        result = result * a         
     return result
