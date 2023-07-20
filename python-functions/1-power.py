cb3012e419046c177930bc28ee4099b8730a0a8b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-07-20 23:09:27 +0200

    updated

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,3 @@
-def pow(a, b): 
-    result = 1
-    if b < 0:
-        a = 1/ a
-        b = abs(b)     
-    for i in range(b): 
-        result = result * a         
-    return round(result, 30)  # Round to 30 decimal places
+#!/usr/bin/python3
+def pow(a, b):
+    return a ** b
