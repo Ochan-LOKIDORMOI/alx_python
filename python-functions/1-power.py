8710b5f3b4714db6dd527a32ce716c55930b9471
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-07-20 22:40:39 +0200

    updated

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
 def pow(a, b):
     result = 1
+
     if b < 0:
         a = 1 / a
         b = -b
 
-    while b > 0:
+    while b:
         if b % 2 == 1:
             result *= a
         a *= a
