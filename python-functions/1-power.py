72172267332f6d870f385403f1f5f1cced4a7202
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-07-20 22:46:37 +0200

    updated

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
+from decimal import Decimal, getcontext
+
 def pow(a, b):
-    result = 1
+    getcontext().prec = 28
+
+    a = Decimal(a)
+    b = Decimal(b)
+    result = Decimal(1)
 
     if b < 0:
-        a = 1 / a
+        a = Decimal(1) / a
         b = -b
 
     while b:
@@ -12,4 +17,4 @@ def pow(a, b):
         a *= a
         b //= 2
 
-    return round(result, 2)
+    return round(float(result), 20)
