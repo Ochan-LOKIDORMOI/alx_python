08c019265e8486d652de2f417df1ca98b02da5c2
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-02 23:18:19 +0200

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/use/bin/python3
 
 class Square:
+    """
+    This class defines a square.
+    
+    """
+
     def __init__(self, size):
+        """
+        The constructor for the Square class.
+        
+        """
         self.__size = size
