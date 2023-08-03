412226e0ac4d365868db7836d4c57e6aed3279e2
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-03 20:03:08 +0200

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,7 +2,7 @@
 """Inherits BaseGeometry class documented now"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
