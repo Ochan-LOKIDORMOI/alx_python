90b9837fda336761c58577ef6d53f57733f9f392
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-09 14:21:09 -0700

    Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL>
-"""
+"""I documented you"""
+
+from urllib.request import urlopen
 import sys
-import requests
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-    
-    try:
-        response = requests.get(url)
-        x_request_id = response.headers.get('X-Request-Id')
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id header not found in the response.")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+if __name__ == '__main__':
+    with urlopen(sys.argv[1]) as res:
+        header = res.info()
+        print(header["X-Request-Id"])
