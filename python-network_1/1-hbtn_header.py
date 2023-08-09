caf7e3209f91af73c2665f743d4ddcb6cb252996
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-09 23:04:41 +0200

    updated

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""This script takes in a URL, sends a request to the URL and displays
+the value of X-Request-Id varialble found in the header
+of the response"""
+
 import requests
 import sys
 
