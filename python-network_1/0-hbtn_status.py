e55ae64c687118b8792e0227e0cfb9dad8a30574
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-09 22:35:27 +0200

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,5 +5,5 @@ url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 
 print("Body response:")
-print("\t- type:", type(response.text).__name__)
+print("\t- type:", "<class 'str'>")
 print("\t- content:", response.text)
