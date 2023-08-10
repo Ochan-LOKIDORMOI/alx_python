92bce3d61ecd9642fe8daf6c74f91d274d8e71a5
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-10 22:06:36 +0200

    code error

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""This script takes in a URL, sends a request to the URL
-and displays the body of the response
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
 """
+import sys
+import requests
+
+
 if __name__ == "__main__":
-    import sys
-    import requests
-    r = requests.get(sys.argv[1])
+    url = sys.argv[1]
+
+    r = requests.get(url)
     if r.status_code >= 400:
-        print("Error code:", r.status_code)
-        sys.exit(1)
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
