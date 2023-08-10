cb66b0eb1a570d1e029c5d936ef2c912df8a4b0e
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-10 22:01:39 +0200

    error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     import sys
     import requests
     r = requests.get(sys.argv[1])
-    if r.status_code > 400:
+    if r.status_code >= 400:
         print("Error code:", r.status_code)
+        sys.exit(1)
     else:
         print(r.text)
