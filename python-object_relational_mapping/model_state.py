0ca65b455cdfe14c45873ff6159e6fd8381e84a1
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-21 00:16:19 -0700

    First state module

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
+
 """
-Lists all State objects from the database hbtn_0e_6_usa.
+    This is a python file that contains the class definition of a
+    State and an instance Base = declarative_base():
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
 
-if __name__ == "__main__":
-    # Create engine, session, and query State objects
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).order_by(State.id).all()
 
-    # Print results
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        State class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
