5383486e474c7649d7dc3d14abc90cf31aaa4913
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-08-29 22:11:31 -0700

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    """
+    Route handler for the root path.
+
+    Returns:
+        str: The message "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
+    """
+    Main entry point of the application.
+    Starts the Flask development server.
+    """
     app.run(host='0.0.0.0', port=5000)
