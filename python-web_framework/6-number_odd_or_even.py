ae9c55783f981292be19ffa87018a450260a2254
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-09-29 23:48:54 +0200

    Updated

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -44,5 +44,13 @@ def number_template(n):
     else:
         return 'Not a number', 404
 
+# Define a route for "/number_odd_or_even/<n>" with strict_slashes=False
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if isinstance(n, int):
+        return render_template('6-number_odd_or_even.html', n=n)
+    else:
+        return 'Not a number', 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
